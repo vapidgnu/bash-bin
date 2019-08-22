5105863badeedc462480d9bf3bf94bf9e73536de
vapidgnu <49007441+vapidgnu@users.noreply.github.com>
2019-08-22 16:20:44 -0700

    Update simple-genkern-update.sh

--- simple-genkern-update.sh
+++ simple-genkern-update.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
-# author: dotc
-# email: oqopo@protonmail.com
-# hosted: vapidgnu
+#  vapidgnu
 #### "WARNING! THIS SCRIPTY IS TIPSY"
 echo "WARNING! THIS SCRIPTY IS TIPSY"
-test -d $1 || echo "${1} not a directory."
+
+test -d $1  || exit 2
+test -d $2 && SRC_DIR=$2 || SRC_DIR="/usr/src"
 
 echo "Press [Enter] to continue! ^c to quit"
 read
-cd /home/dotc/sandbox/usr/src || echo $?
+
+cd /home/dotc/sandbox/usr/src > /dev/null 2>&1 || echo $?
 
 unlink linux > /dev/null 2>&1 || \
         test ! -e linux || \
-         rm -vrI linux || \
+        rm -vrI linux || \
         echo "can't cope with linux"
 
 ln -s $1 linux || echo $?
+
 zcat /proc/config.gz > linux/.config
 zcat /proc/config.gz > linux/config-current
+
 cd
 echo /usr/bin/genkernel --makeopts="-j8" \
         --kernel-config=/home/dotc/sandbox/usr/src/linux/config-current \
         --oldconfig \
         --microcode \
         all
-
 echo grub-mkconfig -o /boot/grub/grub.cfg
+
+
 
 

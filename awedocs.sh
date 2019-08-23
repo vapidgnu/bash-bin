8025c48cccdbc493c417871f6e1163ae3439c557
vapidgnu <49007441+vapidgnu@users.noreply.github.com>
2019-08-22 22:26:06 -0700

    Update awedocs.sh

--- awedocs.sh
+++ awedocs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #local awesome API documentation
-ADOCS='file:////usr/share/doc/awesome-4.2-r2/doc/index.html'
+#ADOCS='file:////usr/share/doc/awesome-4.2-r2/doc/index.html'
+ADOCS='file:////usr/share/doc/$(equery l awesome | cut -d/ -f 2 )/doc/index.html'
 
 surf ${ADOCS}

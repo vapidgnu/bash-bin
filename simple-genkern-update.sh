34fdcde086687edb541ba00fc2675994461e79d9
vapidgnu <49007441+vapidgnu@users.noreply.github.com>
2019-08-22 18:20:35 -0700

    Update simple-genkern-update.sh

--- simple-genkern-update.sh
+++ simple-genkern-update.sh
@@ -3,22 +3,31 @@
 #### "WARNING! THIS SCRIPTY IS TIPSY"
 echo "WARNING! THIS SCRIPTY IS TIPSY"
 
-test -d $1  || exit 3
-test -d $2 && SRC_DIR=$2 || SRC_DIR="/usr/src"
+# test user supplied arguments. 
+test -d $1  || exit 2
+test -d $2 && SRC_DIR=$2 || SRC_DIR="/usr/src" 
+test -d $SRC_DIR || exit 3
 
+# confirm with user
+echo "Attempting to symbolic link ${1} to ${SRC_DIR}/linux"
 echo "Press [Enter] to continue! ^c to quit"
 read
 
+# rid prior linux linux for otherwise; break if unable. 
 cd $SRC_DIR > /dev/null 2>&1 || exit 6
 unlink linux > /dev/null 2>&1 || \
         test ! -e linux || \
-        rm -vrI linux || \
-        echo "can't cope with linux"
+        rm -vrI linux || exit 9
 
+# New src to sybolic linux link
 ln -s $1 linux > /dev/null 2>&1 || exit 13
 
+# copy running systems config
 zcat /proc/config.gz > linux/.config
 zcat /proc/config.gz > linux/config-current
+
+test -r ${SRC_DIR}/linux/config-current -a -r \
+        ${SRC_DIR}/linux/.config || exit 23
 
 cd
 /usr/bin/genkernel \
@@ -27,6 +36,7 @@ cd
         --oldconfig \
 #        --microcode \
         all
+
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
